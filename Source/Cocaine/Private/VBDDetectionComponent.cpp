cc07136f044fc3b79b021d2c9286287c9156fe52
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2026-03-16 03:45:38 +0100

    working on player view range detection

---
+++ Source/Cocaine/Private/VBDDetectionComponent.cpp
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VBDDetectionComponent.h"
+
+#include "CocaineCharacter.h"
+#include "GameFramework/Character.h"
+#include "CollisionLibrary.h"
+
+
+// Sets default values for this component's properties
+UVBDDetectionComponent::UVBDDetectionComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UVBDDetectionComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	// ...
+	
+}
+
+void UVBDDetectionComponent::OnComponentCreated()
+{
+	Super::OnComponentCreated();
+	Character = Cast<ACharacter>( GetOwner());
+	if (!Character)
+	{
+		UE_LOG(LogTemp, Error, TEXT("VBDDetectionComponent: Character not found"));
+	}
+}
+
+
+// Called every frame
+void UVBDDetectionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                           FActorComponentTickFunction* ThisTickFunction)
+{
+	// ...
+}
+
---
+++ Source/Cocaine/Public/VBDDetectionComponent.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "VBDDetectionComponent.generated.h"
+
+
+UCLASS(ClassGroup=(ViewBasedDanger), meta=(BlueprintSpawnableComponent))
+class COCAINE_API UVBDDetectionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+	ACharacter* Character;
+
+public:
+	// Sets default values for this component's properties
+	UVBDDetectionComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	virtual void OnComponentCreated() override;
+
+public:
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
