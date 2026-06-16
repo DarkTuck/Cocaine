47c7a8f0d7359459a9fe4bddaefe9278e4b5e97f
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2026-06-16 18:06:26 +0200

    Now debug will display only in editor

--- Source/Cocaine/Private/CocaineCashGameInstanceSubsystem.cpp
+++ Source/Cocaine/Private/CocaineCashGameInstanceSubsystem.cpp
@@ -6,5 +6,5 @@
 void UCocaineCashGameInstanceSubsystem::AddCash(const int Amount)
 {
 	Cash+=Amount;
-	GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Cash: %d"),Cash));
+	if (WITH_EDITOR) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Cash: %d"),Cash));
 }
